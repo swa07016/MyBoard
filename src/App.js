864e608cf40f17f6a1f754d0568dbd89e89ff9d4
swa07016 <swa07016@naver.com>
2020-02-25 19:21:04 +0900

    Create NavgationBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Example from './NavigationBar';
 
 function App() {
   return (
-   <div>hello</div>
+   <div>
+     <Example></Example>
+   </div>
   );
 }
 
---
+++ src/NavigationBar.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">My board</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Button color="link">Join</Button>{''}
+            </NavItem>
+            <NavItem>
+              <Button color="link">login</Button>{''}            
+            </NavItem>
+          </Nav>
+          <NavbarText>user name</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
